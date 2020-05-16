d1c8b04761bd3c538fbeef37e345b1417bc0f0c6
Priecn <prince.silverarrow@gmail.com>
2020-05-17 00:31:08 +0530

    initial commit

---
+++ src/main/java/com/learn/jenkin2/Jenkin2FreestyleApplication.java
@@ -0,0 +1,13 @@
+package com.learn.jenkin2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkin2FreestyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkin2FreestyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/jenkin2/Jenkin2FreestyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.jenkin2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkin2FreestyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
